6162052bb2a18b67b392ff0dd4daace23b520190
Herman Schubert <dochronosplus@gmail.com>
2025-06-03 15:51:42 -0300

    Eliminado index.tsx: reemplazado por App Router en page.tsx

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CardForm } from "@/components/form/CardForm";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-8 bg-dark1 text-white">
-      <CardForm />
-    </main>
-  );
-}
